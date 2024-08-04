4564e62cc0d36cc775fe0cae2bc4edf230bb9a5b
CatherineAnnFernandez <catherine.ann@pearldatadirect.com>
2024-08-04 18:07:49 +0400

    Delete flight_booking directory

--- flight_booking/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'cathycerine101'
-    MYSQL_DB = 'flight_booking'
-    MYSQL_HOST = 'localhost'
-    MYSQL_PORT = 3306
--- flight_booking/utils/auth.py
+++
@@ -1,8 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def generate_hash(password):
-    return generate_password_hash(password)
-
-def verify_hash(password, hashed):
-    return check_password_hash(hashed, password)
-
